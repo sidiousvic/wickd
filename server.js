f5ed63c6881935b501dc7c31c156d6b0244b4fca
sidiousvic <victor6rene@gmail.com>
2019-05-25 18:03:56 +0900

    Define routes, endpoints and create servers

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-const express = require("express");
+import express from "express";
+import connectDB from "./config/db";
 
 const app = express();
 
+// Connect database
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+
+//Create endpoints/route handlers
 app.get("/", (req, res) => res.send("API Running"));
+
+// Define routes
+app.use("/api/users", require("./routes/api/users").default);
+app.use("/api/auth", require("./routes/api/auth").default);
+app.use("/api/profile", require("./routes/api/profile").default);
+app.use("/api/posts", require("./routes/api/posts").default);
 
 const PORT = process.env.PORT || 5000;
 
