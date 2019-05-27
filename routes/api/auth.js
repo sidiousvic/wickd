b5de89f4792381c08423ba7bdd729e72f9af953a
sidiousvic <victor6rene@gmail.com>
2019-05-27 21:23:39 +0900

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,19 @@
 import { Router } from "express";
 const router = Router();
+import auth from "../../middleware/auth";
+import User from "../../models/Users";
 
 // @route       GET api/users
 // @desc        Test route
 // @accesss     Public
-router.get("/", (req, res) => res.send("Auth route"));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
 
 export default router;
