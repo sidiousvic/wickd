17f7d5c8fe3c90f46720bf565ad6ceefb233e562
sidiousvic <victor6rene@gmail.com>
2019-05-28 21:07:31 +0900

    Built user login with JWT tokens auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 const router = Router();
 import auth from "../../middleware/auth";
-import User from "../../models/Users";
+import User from "../../models/User";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import config from "config";
+const { check, validationResult } = require("express-validator/check");
 
 // @route       GET api/users
 // @desc        Test route
@@ -17,3 +21,68 @@ router.get("/", auth, async (req, res) => {
 });
 
 export default router;
+
+// @route       POST api/auth
+// @desc        Authenticate user and get token
+// @accesss     Public
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please input a password with 6 or more characters",
+      "Password is required"
+    ).exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      // Return jsonwebtoken
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server error");
+    }
+
+    // console.log(req.body);
+    // res.send("User route");
+  }
+);
