1d4b3ff4781ce3b9d620951e6a16046d092afd2f
sidiousvic <victor6rene@gmail.com>
2019-06-12 21:18:26 +0900

    Create "Add post" GET route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,48 @@
 import Router from "express";
 const router = Router();
+import { check, validationResult } from "express-validator/check";
+import auth from "../../middleware/auth";
+import Post from "../../models/Post";
+import Profile from "../../models/Profile";
+import User from "../../models/User";
 
-// @route       GET api/users
-// @desc        Test route
-// @accesss     Public
-router.get("/", (req, res) => res.send("Posts route"));
+// @route       POST api/posts
+// @desc        Create a post
+// @accesss     Private
+router.get(
+  "/",
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        test: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 export default router;
